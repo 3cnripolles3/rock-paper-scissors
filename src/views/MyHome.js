1edfcb86dae5a6a582d60713c8f96ea01cfa2645
cn.ripolles <cn.ripolles@soprasteria.com>
2023-06-08 10:02:33 +0200

    feat(rock-paper-scissors):add MyHome

--- src/views/MyHome.js
+++ src/views/MyHome.js
@@ -1,10 +1,52 @@
 import { html, LitElement } from 'lit';
+import { Router } from '@vaadin/router';
+import { MyHomeStyles } from '../css/MyHome-styles.js';
 
 export class MyHome extends LitElement {
+  static styles = [MyHomeStyles];
 
-  
+  static get properties() {
+    return {
+      titleText: { type: String },
+      userName: { type: String },
+    };
+  }
+
+  constructor() {
+    super();
+    this.titleText = 'Create new player';
+    this.userName = '';
+  }
+
   render() {
-    return html` <div>My home</div> `;
+    return html`
+      <section>
+        <img src="../../../assets/icons/mouse.svg" alt="mouse" />
+        <p>${this.titleText}</p>
+        <div class="inputContainer">
+          <input
+            name="name"
+            id="name"
+            placeholder="Name"
+            type="text"
+            required
+          />
+          <label>Name*</label>
+        </div>
+        <input
+          type="button"
+          @click=${this.handleClick}
+          class="btn"
+          value="JOIN"
+        />
+      </section>
+    `;
+  }
+
+  handleClick() {
+    this.userName = this.shadowRoot.querySelector('#name').value;
+    localStorage.setItem('user', this.userName);
+    if (this.userName !== '') Router.go('/game');
   }
 }
 
